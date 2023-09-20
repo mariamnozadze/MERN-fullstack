0871a32f07a7321884b74d9c99b2f313414bc466
mariamnozadze <mariamnozadze97@yahoo.com>
2023-09-20 13:11:14 +0400

    - Cleaned up environment - added starting files imported axios, redux & etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
-}
+};
 
 export default App;
