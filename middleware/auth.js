f16fd24aa1ba3a54f92edbad4b0ade1a29c6ea06
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-07 12:41:41 +0400

    - Custom Auth Middleware & JWT Verify  part 1

---
+++ middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+//middleware function has excess to the 'req' & 'res', 'next' is callback we need to run, to move onto the next piece
+module.exports = function (req, res, next) {
+
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if(!token) {
+    return res.status(401).json({msg: 'No token, authorization denied'})
+  }
+
+};
