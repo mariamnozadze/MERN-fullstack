a148bb285e8eee05b74bd41d18be2dac02a85a8a
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-08 15:13:41 +0400

    -Custom Auth Middleware & JWT Verify - Verify token

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,4 +12,13 @@ module.exports = function (req, res, next) {
     return res.status(401).json({msg: 'No token, authorization denied'})
   }
 
+  // Verify token
+  try{
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  }catch(err){
+    res.status(401).json({msg: 'Token is not valid'});
+  }
 };
