8cd59e34b3d148ff7b415fdb308a320acf74d847
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-14 10:23:45 +0400

    - Get Current User Profile part 2 - tested logged in user on postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -8,9 +8,16 @@ const User = require("../../models/User");
 // @route     GET api/profile/me
 // @desc      Get current users profile
 // @access    Private
-router.get("/", auth, async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne({ user: req.user.id });
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+    if(!profile) {
+      return res.status(400).json({msg: 'There is no profile for this user'});
+    }
+
+    res.json(profile)
+
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
