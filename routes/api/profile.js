9439be9a883f28c913ddecb37e4b3b0a7d70e25f
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-28 14:12:45 +0400

    - delete profile education - tested it on postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -247,6 +247,8 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   }
 });
 
+
+
 // @route     PUT api/profile/education
 // @desc      Add profile education
 // @access    Private
@@ -298,5 +300,29 @@ router.put(
     }
   }
 );
+
+
+// @route     DELETE api/profile/education/edu_id
+// @desc      Delete education from profile
+// @access    Private
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 
 module.exports = router;
