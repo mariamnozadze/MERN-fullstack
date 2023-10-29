be1da3c441f42af9582525ef6e693f6c146a13a4
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-29 17:14:23 +0400

    - Added post route - tested on postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 
-const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route     POST api/posts
 // @desc      Create a post
@@ -15,17 +15,26 @@ router.post(
   [auth, [check("text", "Text is required").not().isEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()})
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const user = await User.findById(req.user.id).select('-password');
+    try {
+      const user = await User.findById(req.user.id).select("-password");
 
-    const newPost = {
+      const newPost = new Post ({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
     }
   }
 );
