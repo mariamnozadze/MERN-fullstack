e0c799994f6f4be74f09ee361abee067496035d4
mariamnozadze <mariamnozadze97@yahoo.com>
2023-10-30 19:21:41 +0400

    - added values to the body

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -45,6 +45,25 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
   }
 );
 
