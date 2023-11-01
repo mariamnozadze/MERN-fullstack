cdb586f67c4e2cd79c085e39aee9d7241596a3cd
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-01 12:16:34 +0400

    - forgot to save updated profile - tested on postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -236,7 +236,9 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
     // Remove the experience at the found index
     profile.experience.splice(removeIndex, 1);
 
+    await profile.save();
 
+    res.json(profile)
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
