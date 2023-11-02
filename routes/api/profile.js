8b80e11d5bb7698cadc76f1e64f4d62747f758c4
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-02 13:13:41 +0400

    - get github repos for profile -tested on postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -247,8 +249,6 @@ router.delete("/experience/:exp_id", auth, async (req, res) => {
   }
 });
 
-
-
 // @route     PUT api/profile/education
 // @desc      Add profile education
 // @access    Private
@@ -301,7 +301,6 @@ router.put(
   }
 );
 
-
 // @route     DELETE api/profile/education/edu_id
 // @desc      Delete education from profile
 // @access    Private
@@ -324,5 +323,34 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   }
 });
 
+// @route     GET api/profile/github/:username
+// @desc      Get user repos from Github
+// @access    Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No Github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
