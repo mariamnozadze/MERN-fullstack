3924ad3ae763d7acb25b86c09d1175f21cffa076
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-08 12:41:52 +0400

    - get profile by user id - tested on postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,6 +112,8 @@ router.post(
   }
 );
 
+
+
 // @route     GET api/profile
 // @desc      Get all profiles
 // @access    Public
@@ -121,6 +123,30 @@ router.get("/", async (req, res) => {
     res.json(profiles);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
+
+
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get profile by user ID
+// @access    Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
     res.status(500).send("server error");
   }
 });
