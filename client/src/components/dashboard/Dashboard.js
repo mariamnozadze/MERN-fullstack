b42f3b1cbb1dfeb3cad850d2d6e72ef9130d5f39
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-09 15:29:26 +0400

    -get current profile part 2

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
+import profile from "../../reducers/profile";
 
-const Dashboard = props => {
-  return (
-    <section className='container'>
-      Dashboard
-    </section>
-  )
-}
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  return <section className="container">Dashboard</section>;
+};
 
 Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
 
-export default Dashboard
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,14 +1,30 @@
-const initialState= {
-    profile: null,
-    profiles: [],
-    repos: [],
-    loading: true,
-    error: {}
-}
+import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
 
-export default function(state= initialState, action) {
-    const {type, payload} = action;
-    switch(type) {
-        
-    }
+const initialState = {
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
 }
