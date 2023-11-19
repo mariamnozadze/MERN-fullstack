466bb70bf3aeeb7b22d039af95a997bbc3531f01
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-19 12:13:14 +0400

    - Added component starting files -fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Fragment>
+    </Router>
   );
 };
 
 export default App;
+
