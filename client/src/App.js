b4c88d112f8eb39bf21a655fd1f3e7dc490d5c97
mariamnozadze <mariamnozadze97@yahoo.com>
2023-11-28 21:12:45 +0400

    fixing alert component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -20,6 +21,7 @@ const App = () => {
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
           </Routes>
+          <Alert />
         </Fragment>
       </Router>
     </Provider>
