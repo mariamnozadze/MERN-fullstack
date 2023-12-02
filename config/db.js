93cf3c2da6397d07e054d5dfba9dd5f15dfe48e2
mariamnozadze <mariamnozadze97@yahoo.com>
2023-12-02 10:12:41 +0400

    - fixed issues on server side and make it work again AT LAST!

--- config/db.js
+++ config/db.js
@@ -1,15 +1,21 @@
 const mongoose = require("mongoose");
 const config = require("config");
+
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, 
+      useFindAndModify: false,
+    });
 
     console.log("MongoDB connected....");
   } catch (err) {
     console.error(err.message);
-    //exit process with failure
+    // exit process with failure
     process.exit(1);
   }
 };
