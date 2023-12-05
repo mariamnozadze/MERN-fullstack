6583bff365497631c546c49b9a2c3c3f1c192247
mariamnozadze <mariamnozadze97@yahoo.com>
2023-12-05 13:34:25 +0400

    - added logout proptypes

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import { setAlert } from "./alert";
 import setAuthToken from "../utils/setAuthToken";
@@ -93,3 +94,8 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+// Logout / Clear profile
+export const logout = () => dispatch => {
+  dispatch({type: LOGOUT})
+}
