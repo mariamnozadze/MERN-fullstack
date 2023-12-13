5fe03d078632edd50300f91d0aec189211739a8b
mariamnozadze <mariamnozadze97@yahoo.com>
2023-12-13 14:18:24 +0400

    - changed classes to classnames cause i forgot

--- client/src/components/profile-form/AddExperience.js
+++ client/src/components/profile-form/AddExperience.js
@@ -17,37 +17,37 @@ const AddExperience = (props) => {
     })
 
   return (
-    <section class="container">
-      <h1 class="large text-primary">Add An Experience</h1>
-      <p class="lead">
-        <i class="fas fa-code-branch"></i> Add any developer/programming
+    <section className="container">
+      <h1 className="large text-primary">Add An Experience</h1>
+      <p className="lead">
+        <i className="fas fa-code-branch"></i> Add any developer/programming
         positions that you have had in the past
       </p>
       <small>* = required field</small>
-      <form class="form">
-        <div class="form-group">
+      <form className="form">
+        <div className="form-group">
           <input type="text" placeholder="* Job Title" name="title" required />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input type="text" placeholder="* Company" name="company" required />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <input type="text" placeholder="Location" name="location" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <h4>From Date</h4>
           <input type="date" name="from" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <p>
             <input type="checkbox" name="current" value="" /> Current Job
           </p>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <h4>To Date</h4>
           <input type="date" name="to" />
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <textarea
             name="description"
             cols="30"
@@ -55,8 +55,8 @@ const AddExperience = (props) => {
             placeholder="Job Description"
           ></textarea>
         </div>
-        <input type="submit" class="btn btn-primary my-1" />
-        <Link class="btn btn-light my-1" href="/dashboard">
+        <input type="submit" className="btn btn-primary my-1" />
+        <Link className="btn btn-light my-1" href="/dashboard">
           Go Back
         </Link>
       </form>
