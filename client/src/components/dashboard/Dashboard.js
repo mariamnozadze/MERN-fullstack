5794fd1b040cfb9e9d43ad7be5220a8c0ae84574
mariamnozadze <mariamnozadze97@yahoo.com>
2023-12-17 10:21:13 +0400

    - forgot to add welcome text

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,12 +5,25 @@ import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import profile from "../../reducers/profile";
 
-const Dashboard = ({ getCurrentProfile, auth, profile: {profile, loading} }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
-  return loading && profile === null ? <Spinner /> : <section className="container">Dashboard</section>
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <section className="container">
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+      </p>
+    </section>
+  );
 };
 
 Dashboard.propTypes = {
