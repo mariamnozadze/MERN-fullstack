7a9620ff0feafc4f58be1360b71a74756a654c9a
mariamnozadze <mariamnozadze97@yahoo.com>
2023-12-18 11:12:41 +0400

    - added profile cleanup function after logout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
-import profile from "../../reducers/profile";
+import { Link } from "react-router-dom";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -22,6 +22,14 @@ const Dashboard = ({
       <p className="lead">
         <i className="fas fa-user"></i> Welcome {user && user.name}
       </p>
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="/create-profile" className="btn btn-primary my-1"> Create profile</Link>
+        </Fragment>
+      )}
     </section>
   );
 };
