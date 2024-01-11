f95da4f479c77076c94281cda94003f8573a0f10
mariamnozadze <mariamnozadze97@yahoo.com>
2024-01-11 13:18:59 +0400

    - added frontend to component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,26 +1,50 @@
-import React, { useEffect } from 'react'
-import PropTypes from 'prop-types'
-import Spinner from '../layout/Spinner'
-import { getProfiles } from '../../actions/profile'
-import { connect } from 'react-redux'
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import Spinner from "../layout/Spinner";
+import { getProfiles } from "../../actions/profile";
+import { connect } from "react-redux";
+import profile from "../../reducers/profile";
+import ProfileItem from "./ProfileItem";
 
-const Profiles = ({getProfiles, profile:{profiles, loading}})=> {
-    useEffect(()=> {
-        getProfiles()
-    }, [])
+const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
   return (
-    
-    <div>Profiles</div>
-  )
-}
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <div className="container">
+            <h1 className="large text-primary">Developers</h1>
+            <p className="lead">
+              <i className="fab fa-connectdevelop"></i>
+              Browse and connect with developers
+            </p>
+            <div className="profiles">
+              {profiles.length > 0 ? (
+                profiles.map((profile) => (
+                  <ProfileItem key={profile._id} profile={profile} />
+                ))
+              ) : (
+                <h4>No profiles found...</h4>
+              )}
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 Profiles.propTypes = {
-    getProfiles: PropTypes.func.isRequired,
-    profile: PropTypes.object.isRequired,
-}
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-    profile: state.profile
-})
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+});
 
-export default connect(mapStateToProps, {getProfiles})(Profiles)
+export default connect(mapStateToProps, { getProfiles })(Profiles);
